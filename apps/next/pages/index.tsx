a538e85c5f6fa4be0bc60a1091bc326524334c40
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-05-03 11:14:16 -0300

    fix jest

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
 
-import { Button } from 'design-system/src/components/atoms/Button'
 import type { SolitoAppProps } from 'solito'
 
 export default (_props: SolitoAppProps['pageProps']) => {
-  return (
-    <>
-      <Button>asd</Button>
-    </>
-  )
+  return <></>
 }
