eb5dbaedda8315432a3309ce17a9630a005c2786
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-05-03 01:26:45 -0300

    expo working!

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import { Button } from 'design-system/src/components/atoms/Button'
 import type { SolitoAppProps } from 'solito'
 
 export default (_props: SolitoAppProps['pageProps']) => {
-  return <></>
+  return (
+    <>
+      <Button>asd</Button>
+    </>
+  )
 }
